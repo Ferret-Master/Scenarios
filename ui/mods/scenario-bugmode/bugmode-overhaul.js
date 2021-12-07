bef01687010f69597d2eacc1a163d537b8702d06
Ferret-Master <64487611+Ferret-Master@users.noreply.github.com>
2021-12-07 21:55:03 +0800

    disabled old loadouts and testing scenarios

--- ui/mods/scenario-bugmode/bugmode-overhaul.js
+++ ui/mods/scenario-bugmode/bugmode-overhaul.js
@@ -1,8 +1,20 @@
 //overhaul uses hive mode as a base but requires different internals so will be seperate file wise
+var bugStatus = {
+    landTime:null,
+    bugUnlocks:[],
+    bugBuffs:[],
+    neutralEvents:[],//this would be a list of objects, as each event will have a name/location
+    playerController:0
 
+}
 
+var playerUptime = []//tracks each players uptime and uses this to assign controller status, goes uptime then slot if even uptime
 //for central control can either make it regular update based or detect disconnects
 function controllerMissing(){//if assumed controller has not sent out update in x time set yourself to controller if your uptime is highest
+
+}
+
+function assignController(){
 
 }
 
@@ -14,8 +26,35 @@ function updatePlayers(){//controller sends out a json copy of the current games
 
 }
 
+
 function sendCommandToPlayers(){//director events need to be communicated to players to trigger puppets/ui stuff
 
 }
 
-function notEmpty(){}
+
+function playerDirector(){//controls rewards for struggling players/over time. time/metal loss based
+
+}
+
+function aggressionBehaviour(){//based on the bugs aggression value it tweaks values used for expansion/wave spawning
+
+}
+
+function aiDirector(){//based on time/destroyed bug structures, will cause both events and upgrades/buffs for the bugs
+
+
+}
+
+function neutralDirector(){//performs events that are not directly related to bugs. usually at the start of the game for variety
+
+}
+
+function expansionController(){}
+
+function waveController(){}
+
+function mainLoop(){
+    if(controllerMissing){
+        
+    }
+}
